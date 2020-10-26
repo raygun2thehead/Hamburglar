97ea3897d73c6faec552e4bf8f1af1674c37b634
Steven Schaab <stevenes83@gmail.com>
2020-10-25 19:09:16 -0500

    can change status of sql object. just needs readme

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -20,26 +20,39 @@ router.post("/api/burger", function(req, res) {
   });
 });
 
-// router.put("/api/burger/:id", function(req, res) {
-//   var condition = "id = " + req.params.id;
+router.put("/api/burger/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
 
-//   console.log("condition", condition);
+  console.log("condition", condition);
 
-//   burger.update(
-//     {
-//       devoured: req.body.devoured
-//     },
-//     condition,
-//     function(result) {
-//       if (result.changedRows === 0) {
-//         // If no rows were changed, then the ID must not exist, so 404
-//         return res.status(404).end();
-//       }
-//       res.status(200).end();
+  burger.update(
+    {
+      devoured: req.body.devoured
+    },
+    condition,
+    function(result) {
+      if (result.changedRows === 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      }
+      res.status(200).end();
 
-//     }
-//   );
-// });
+    }
+  );
+});
+
+router.delete("/api/burger/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  burger.delete(condition, function(result) {
+    if (result.affectedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
 
 // Export routes for server.js to use.
 module.exports = router;
--- public/assets/burgers.js
+++ public/assets/burgers.js
@@ -1,43 +1,55 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
-$(function() {
-    // $(".change-sleep").on("click", function(event) {
-    //   var id = $(this).data("id");
-    //   var newSleep = $(this).data("newsleep");
-  
-    //   var newSleepState = {
-    //     sleepy: newSleep
-    //   };
-  
-    //   // Send the PUT request.
-    //   $.ajax("/api/cats/" + id, {
-    //     type: "PUT",
-    //     data: newSleepState
-    //   }).then(
-    //     function() {
-    //       console.log("changed sleep to", newSleep);
-    //       // Reload the page to get the updated list
-    //       location.reload();
-    //     }
-    //   );
-    // });
-  
-    $(".create-form").on("submit", function(event) {
-      event.preventDefault();
-  
-      var burger_name = {
-        name: $("#burd").val().trim(),
-      };
-  
-      $.ajax("/api/burger", {
-        type: "POST",
-        data: burger_name
-      }).then(
-        function() {
-          console.log("created new burger");
-          location.reload();
-        }
-      );
-    });
-  
+$(function () {
+  $(".devour").on("click", function (event) {
+    var id = $(this).data("id");
+    // var newDevour = $(this).data("newdevour");
+
+    var newDevouredState = {
+      devoured: true
+    };
+
+    // Send the PUT request.
+    $.ajax("/api/burger/" + id, {
+      type: "PUT",
+      data: newDevouredState
+    }).then(
+      function () {
+        // console.log("changed devoured to", newDevoured);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
   });
-  
+
+  $(".create-form").on("submit", function (event) {
+    event.preventDefault();
+
+    var burger_name = {
+      name: $("#burd").val().trim(),
+    };
+
+    $.ajax("/api/burger", {
+      type: "POST",
+      data: burger_name
+    }).then(
+      function () {
+        console.log("created new burger");
+        location.reload();
+      }
+    );
+  });
+  $(".delete").on("click", function(event) {
+    var id = $(this).data("id");
+
+    // Send the DELETE request.
+    $.ajax("/api/burger/" + id, {
+      type: "DELETE"
+    }).then(
+      function() {
+        console.log("deleted burger", id);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+});
